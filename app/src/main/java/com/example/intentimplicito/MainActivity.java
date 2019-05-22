a4ac5a7e77d982d90e3890e3e173e3bfc82a336c
Fernando López Carballeda <flc.2056@zohomail.eu>
2019-05-22 09:52:54 +0200

    Añadidos los comentarios y pequeñas mejoras en el código.

--- app/src/main/java/com/example/intentimplicito/MainActivity.java
+++ app/src/main/java/com/example/intentimplicito/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private int originalWidth;
-    private static int DURATION = 1000;
+    private static final int DURATION = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,41 +27,68 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buscar(View v) {
+        // Tomo el objeto que necesito animar
         final EditText editText = findViewById(R.id.editText);
+        // Compruebo si está expandido
         if (editText.getWidth() == originalWidth) { // Debo animar el expand
-            final TextView textView = findViewById(R.id.textViewLabel);
+            TextView textView = findViewById(R.id.textViewLabel);
+            // Tomo el campo TextView como referencia para el tamaño
             final ConstraintLayout.LayoutParams lparams = (ConstraintLayout.LayoutParams) editText.getLayoutParams();
+            // Creo una animación de in Entero entre los valores
             ValueAnimator anim = ValueAnimator.ofInt(editText.getWidth(), textView.getWidth());
+            // Pongo el CallBack a la nueva clase que se crea para esta animación.
             anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                 @Override
                 public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                    // En este método tomo el nuevo valor que va tomando el Entero
+                    // animado
                     int val = (Integer) valueAnimator.getAnimatedValue();
+                    // Lo asigno a la Constraint que quiero animar
                     lparams.width = val;
+                    // Actualizo el valor en el objeto EditText animado.
                     editText.setLayoutParams(lparams);
                 }
             });
+            // Establezco la duración de la animación
             anim.setDuration(DURATION);
+            // Lanzo la animación.
             anim.start();
         } else {
+            // Ya está expandido
             String texto = editText.getText().toString();
-            if (texto.isEmpty()) {
+            if (texto.isEmpty()) { // No tengo texto, debo colapsar
+                // Tomo el valor de las constraint que tiene el objeto EditText en este momento.
                 final ConstraintLayout.LayoutParams lparams = (ConstraintLayout.LayoutParams) editText.getLayoutParams();
+                // Creo una animación de un valor Entero (Integer o Int)
                 ValueAnimator anim = ValueAnimator.ofInt(editText.getWidth(), originalWidth);
+                // Pongo el CallBack a la nueva clase que se crea para esta animación.
                 anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                     @Override
                     public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        // En este método tomo el nuevo valor que va tomando el Entero
+                        // animado
                         int val = (Integer) valueAnimator.getAnimatedValue();
+                        // Lo asigno a la Constraint que quiero animar
                         lparams.width = val;
-                        editText.setLayoutParams( lparams);
+                        // Actualizo el valor en el objeto EditText animado.
+                        editText.setLayoutParams(lparams);
                     }
                 });
+                // Establezco la duración de la animación
                 anim.setDuration(DURATION);
+                // Lanzo la animación.
                 anim.start();
             } else {
-                Log.d("MYAPP", Integer.toString(editText.getWidth()) + " Expandido");
+                // Tengo un texto a buscar, creo el Intent Implicito
+                // para que el Teléfono utilice la aplicación que pueda
+                // realizar esta funcionalidad
                 Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                // Añado el texto a buscar al Intent
                 intent.putExtra(SearchManager.QUERY, texto);
+                // Compruebo que tengo alguna aplicación que puede realizar
+                // Esta acción.
                 if (intent.resolveActivity(getPackageManager()) != null) {
+                    // Lanzo el Intent para que el Teléfono realice la función solicitada.
                     startActivity(intent);
                 }
             }
