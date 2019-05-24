fc76d78faf27de418b4624d898a1b19871350e7f
Fernando López Carballeda <flc.2056@zohomail.eu>
2019-05-24 10:56:13 +0200

    Animación de una image entrando desde la izquierda.

--- app/src/main/java/com/example/intentimplicito/Animation2Activity.java
+++ app/src/main/java/com/example/intentimplicito/Animation2Activity.java
@@ -4,19 +4,23 @@ import android.animation.ObjectAnimator;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 
 public class Animation2Activity extends AppCompatActivity {
 
     private static final int DURATION = 1000;
     private ImageView image;
+    private float rightInitialPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation2);
         image = findViewById( R.id.imageViewLion);
-        ObjectAnimator animation = ObjectAnimator.ofFloat(image, "translationX", -1000f);
+        ConstraintLayout.LayoutParams lparams = (ConstraintLayout.LayoutParams)image.getLayoutParams();
+        rightInitialPosition = lparams.rightMargin;
+        ObjectAnimator animation = ObjectAnimator.ofFloat(image, "translationX", -600f);
         animation.setDuration(2);
         animation.start();
     }
@@ -24,5 +28,9 @@ public class Animation2Activity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        ObjectAnimator animation = ObjectAnimator.ofFloat(image, "x", -600f, 0f);
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.setDuration(20000);
+        animation.start();
     }
 }
