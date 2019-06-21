1db3a9752db064cf740f29cd279b5cd3925c6ba7
Fernando López Carballeda <flc.2056@zohomail.eu>
2019-06-21 09:36:02 +0200

    Empezando tercera animación con imagen scale And fadeout. Terminado.

--- app/src/main/java/com/example/intentimplicito/Animation3Activity.java
+++ app/src/main/java/com/example/intentimplicito/Animation3Activity.java
@@ -1,5 +1,8 @@
 package com.example.intentimplicito;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,13 +20,41 @@ public class Animation3Activity extends AppCompatActivity {
         scaleView(v, 1.5f, 10f);
     }
     private void scaleView(View v, float startScale, float endScale) {
-        Animation anim = new ScaleAnimation(
-                startScale, endScale, // Start and end values for the X axis scaling
-                startScale, endScale, // Start and end values for the Y axis scaling
-                Animation.RELATIVE_TO_SELF, 0.5f, // Pivot point of X scaling
-                Animation.RELATIVE_TO_SELF, 0.5f); // Pivot point of Y scaling
-        anim.setFillAfter(true); // Needed to keep the result of the animation
-        anim.setDuration(10000);
-        v.startAnimation(anim);
+        final Animator scaleAnimatorX = ObjectAnimator
+                .ofFloat( v, "scaleX", 1,5f)
+                .setDuration(1000);
+        final Animator scaleAnimatorY = ObjectAnimator
+                .ofFloat( v, "scaleY", 1,5f)
+                .setDuration(1000);
+
+        final AnimatorSet animatorInitialSet = new AnimatorSet();
+        animatorInitialSet.playTogether(
+                scaleAnimatorX,
+                scaleAnimatorY
+        );
+
+        final Animator alphaAnimator = ObjectAnimator
+                .ofFloat(v, View.ALPHA, 1f, 0f)
+                .setDuration(5000);
+        final Animator scaleAnimatorX2 = ObjectAnimator
+                .ofFloat( v, "scaleX", 10f)
+                .setDuration(5000);
+        final Animator scaleAnimatorY2 = ObjectAnimator
+                .ofFloat( v, "scaleY", 10f)
+                .setDuration(5000);
+
+        final AnimatorSet animatorFinalSet = new AnimatorSet();
+        animatorInitialSet.playTogether(
+                scaleAnimatorX2,
+                scaleAnimatorY2,
+                alphaAnimator
+        );
+
+        final AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playSequentially(
+                animatorInitialSet,
+                animatorFinalSet
+        );
+        animatorSet.start();
     }
 }
